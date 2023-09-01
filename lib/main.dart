861ee49ef650ecab27d37f917d064cb7dbc88fcb
menna550 <mennag776@gmail.com>
2023-09-01 10:29:25 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:sports_app/Screens/Home_Screen.dart';
 import 'package:sports_app/Screens/OnBoardingScreen/On_Boarding_Screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
+        
         home: showHome ? home_screen() : OnBoardingScreen(),
       ),
     );
